3b9388555c6e17ca2469f548e6fa89e3026e6b12
Maksim Andreev <chemaks@mail.ru>
2024-03-12 16:59:12 -0400

    andreev_homework_3_1

--- homework/maksim_andreev/Homework_3/zadanie_1.py
+++ homework/maksim_andreev/Homework_3/zadanie_1.py
@@ -1,8 +1,8 @@
 a = 4
 b = 8
-s = a + b # summa
-d = a - b # raznica
-p = a * b # proizvedenie
+s = a + b  # summa
+d = a - b  # raznica
+p = a * b  # proizvedenie
 
 print("Summa:", s)
 print("Raznica:", d)
