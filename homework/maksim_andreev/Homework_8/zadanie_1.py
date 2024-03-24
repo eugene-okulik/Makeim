81cc90df30770a690a5ad973ece65b7f3f8ca551
Maksim Andreev <chemaks@mail.ru>
2024-03-23 22:05:07 -0400

    andreev_homework_8

---
+++ homework/maksim_andreev/Homework_8/zadanie_1.py
@@ -0,0 +1,11 @@
+import random
+
+bonus = random.choice([True, False])
+salary = int(input('Enter your salary: '))
+sumofbonus = random.randint(1, 10000)
+
+if bonus == True:
+    equals = salary + sumofbonus
+else:
+    equals = salary
+print(f'{salary}, {bonus} - ${equals}')
