2a812c2b5c7a8889e3c1e227bf91fab64b6cee52
Maksim Andreev <chemaks@mail.ru>
2024-03-23 22:09:06 -0400

    andreev_homework_8_flake

--- homework/maksim_andreev/Homework_8/zadanie_1.py
+++ homework/maksim_andreev/Homework_8/zadanie_1.py
@@ -4,7 +4,7 @@ bonus = random.choice([True, False])
 salary = int(input('Enter your salary: '))
 sumofbonus = random.randint(1, 10000)
 
-if bonus == True:
+if bonus is True:
     equals = salary + sumofbonus
 else:
     equals = salary
