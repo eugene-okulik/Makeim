d55d85f55dfc9dd777e6bba238d687a5276cf45e
Maksim Andreev <chemaks@mail.ru>
2024-04-02 14:32:52 -0400

    andreev_homework_11

---
+++ homework/maksim_andreev/Homework_11/zadanie_1.py
@@ -0,0 +1,28 @@
+class Books:
+    material = 'бумага'
+    presence_of_text = True
+
+    def __init__(self, book_name, author, count_pages, isbn, reserved_status=False):
+        self.book_name = book_name
+        self.author = author
+        self.count_pages = count_pages
+        self.isbn = isbn
+        self.reserved_status = reserved_status
+
+
+book_1 = Books('Идиот', 'Достоевский', '500', '2-266-11156-6')
+book_2 = Books('Война и мир', 'Толстой', '1010', '978-5080043987')
+book_3 = Books('Мастер и Маргарита', 'Булгаков', '650', '978-5080043981')
+book_4 = Books('Маленький принц', 'Экзюпери', '259', '978-5082043987')
+book_5 = Books('Метро 2033', 'Глуховский', '384', '2-266-22226-6')
+
+books = [book_1, book_2, book_3, book_4, book_5]
+book_5.reserved_status = True
+
+for book in books:
+    if book.reserved_status:
+        print(f'Название: ' + book.book_name + ', Автор: ' + book.author + ', страниц: ' + book.count_pages +
+              ', материал: ' + book.material + ', зарезервирована')
+    else:
+        print(f'Название: ' + book.book_name + ', Автор: ' + book.author + ', страниц: ' + book.count_pages +
+              ', материал: ' + book.material)
---
+++ homework/maksim_andreev/Homework_11/zadanie_2.py
@@ -0,0 +1,36 @@
+class Books:
+    material = 'бумага'
+    presence_of_text = True
+
+    def __init__(self, book_name, author, count_pages, isbn, reserved_status=False):
+        self.book_name = book_name
+        self.author = author
+        self.count_pages = count_pages
+        self.isbn = isbn
+        self.reserved_status = reserved_status
+
+
+class Textbooks(Books):
+    avail_task = True
+
+    def __init__(self, book_name, author, count_pages, isbn, subject, group, reserved_status=False):
+        super().__init__(book_name, author, count_pages, isbn, reserved_status)
+        self.subject = subject
+        self.group = group
+
+
+textbook_1 = Textbooks('Алгебра', 'Иванов', '180', '2-262-11256-6', 'Математика', '10')
+textbook_2 = Textbooks('География России', 'Паркшатов', '250', '978-2210043987', 'География', '7')
+textbook_3 = Textbooks('История Руси', 'Иванина', '432', '911-5080043981', 'История', '9')
+textbook_4 = Textbooks('Обществознание', 'Марцун', '201', '978-5555543987', 'Обществознание и Право', '11')
+
+textbooks = [textbook_1, textbook_2, textbook_3, textbook_4]
+textbook_3.reserved_status = True
+
+for textbook in textbooks:
+    if textbook.reserved_status:
+        print(f'Название: ' + textbook.book_name + ', Автор: ' + textbook.author + ', страниц: ' + textbook.count_pages
+              + ', предмет: ' + textbook.subject + ', класс: ' + textbook.group + ', зарезервирована')
+    else:
+        print(f'Название: ' + textbook.book_name + ', Автор: ' + textbook.author + ', страниц: ' + textbook.count_pages
+              + ', предмет: ' + textbook.subject + ', класс: ' + textbook.group)
