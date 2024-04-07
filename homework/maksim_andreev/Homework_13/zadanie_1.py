bdbc00c1ce1e8ba86e774b55be283cb98307ed18
Maksim Andreev <chemaks@mail.ru>
2024-04-07 12:57:23 -0400

    andreev_homework_13

--- homework/maksim_andreev/Homework_13/zadanie_1.py
+++ homework/maksim_andreev/Homework_13/zadanie_1.py
@@ -18,10 +18,10 @@ with open(egn_filepath) as hw_file:
         if parts[0].startswith('1'):
             new_date = date_time + timedelta(weeks=1)
             print(f'The date {date_time} plus one week =  {new_date.strftime(date_format)}')
-        if parts[0].startswith('2'):
+        elif parts[0].startswith('2'):
             day_of_week = date_time.strftime('%A')
             print(f'Weekday of the date {date_time} - {day_of_week}')
-        if parts[0].startswith('3'):
+        elif parts[0].startswith('3'):
             today = datetime.now()
             diff = (today - date_time).days
             print(f'Passed {diff} days from {date_time}')
