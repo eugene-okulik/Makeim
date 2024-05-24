4e1aa0fe2c92cd23cc149b058cfbb395132b0e29
Maksim Andreev <chemaks@mail.ru>
2024-05-24 21:46:17 +0600

    andreev_homework_19

--- homework/maksim_andreev/Homework_19/test_pytest_zadanie.py
+++ homework/maksim_andreev/Homework_19/test_pytest_zadanie.py
@@ -21,8 +21,9 @@ def post_and_delete():
     body = {"name": "Apple MacBook Pro 16", "data": {"year": 2019, "price": 1849.99,
             "CPU model": "Intel Core i9", "Hard disk size": "1 TB"}}
     headers = {"content-type": "application/json"}
-    response = requests.post('https://api.restful-api.dev/objects', json=body, headers=headers).json()
-    post_id = response['id']
+    response = requests.post('https://api.restful-api.dev/objects', json=body, headers=headers)
+    assert response.status_code == 200
+    post_id = response.json()['id']
     yield post_id
     requests.delete(f'https://api.restful-api.dev/objects/{post_id}')
 
